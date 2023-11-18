1120f2d9f1e03c535e500a744441a02c01e5bd93
MaryamKhanmadova <maryamkhanmadov@gmail.com>
2023-11-18 17:23:40 +0400

    update

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -13,20 +13,22 @@ describe('template spec', () => {
     cy.get('a[href="/az/individuals/loans/"]').first().click();
     cy.get('a[class="bl_arr_bttn"]').first().click();
 
+    let i = 0;
+    const input = ["name", "surname", "salary", "work", "credit"];
+    let values = ["Maryam", "Khanmadova", "500", "teacher"];
+
     cy.get('.cd_i_desc').each(($element => {
       if ($element[0].children[0].innerText === "Məbləğ") {
         var mebleg_arr = $element[0].children[1].innerText.split("-");
         var min_mebleg = mebleg_arr[0].trim();
-        cy.get("input[name='credit']").clear().type(min_mebleg - 1)
+        cy.get(`input[name='${input[4]}']`).clear().type(min_mebleg - 1)
       }
     }))
 
-    const input = ["name", "surname", "salary", "work"];
-    let values = ["Maryam", "Khanmadova", "500", "teacher", '070'];
-    let i = -1;
     for (const name of input) {
-      i++;
       cy.get(`input[name='${name}']`).should('be.enabled').type(values[i]);
+      i++;
+      if (i == 4) break;
     }
 
     cy.get("[name='prefix']").select('070');
